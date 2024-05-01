0b0ffcfc69e6ec5d9d67fd86ddd5ba0739854b30
thduy-benj <benjaminnguyen321@gmail.com>
2024-05-01 12:14:54 +0700

    Update Yolo.py

--- Yolo.py
+++ Yolo.py
@@ -1,19 +1,14 @@
 import os
 import cv2
 import time
-import base64
 import threading
-import urllib.request
 from ultralytics import YOLO
 from datetime import datetime
-from multiprocessing import Process
 from paho.mqtt import client as mqtt_client
 
 model=YOLO("yolov5nu.pt")
 broker='broker.hivemq.com'
 port=1883
-# Định dạng của topic: /detected/day-month-year/ID/
-# client_id='your client id'
 threshold=0.7 # Ngưỡng chấp nhận để gửi ảnh (%)
 
 # Hàm gửi ảnh (lưu vào local và gửi qua mqtt)
@@ -23,8 +18,11 @@ def send_image(box,frame):
     crop_person = frame[y1:y2,x1:x2] # cắt phần ảnh có người
     id_person = str(int(box.id[0]))
     now = datetime.now()
-    year,month,day,hour,minute,second = str(now.year),str(now.month),str(now.day),str(now.hour),str(now.minute),str(now.second) 
-    file_path = ".\\detected\\" + day + "-" + month + "-" + year # mỗi ngày là một thư mực
+    year,month,day,hour,minute,second = str(now.year),str(now.month),str(now.day),str(now.hour),str(now.minute),str(now.second)
+    file_path = ".\\detected" # Tạo thư mục detect
+    if os.path.exists(file_path) == False:
+        os.mkdir(file_path) 
+    file_path += "\\" + day + "-" + month + "-" + year # mỗi ngày là một thư mực
     if os.path.exists(file_path) == False:
         os.mkdir(file_path)
     file_path += "\\" + "ID" + id_person # thư mục mỗi ngày chứa các thư mục ID người
@@ -32,7 +30,7 @@ def send_image(box,frame):
         os.mkdir(file_path)
 
     # cái này để lưu ảnh vào local 
-    image_name = file_path + "\\" + hour + "-" + minute + "-" + second + ".jpg"
+    image_name = file_path + "\\" + hour + "-" + minute + "-" + second + "conf " + str(float(box.conf)) + ".jpg"
     is_saved = cv2.imwrite(image_name,crop_person)
     if is_saved: print("ảnh đã được lưu") 
     else: print("ERROR")
@@ -66,6 +64,7 @@ def publish(client,byteArr,topic):
 if __name__ == '__main__':
     cam = cv2.VideoCapture(0)
     ret = True
+    detected_person = dict()
     while ret:
         ret, frame = cam.read()
         results = model.track(frame, stream=True, persist=True)
@@ -74,9 +73,18 @@ if __name__ == '__main__':
             for box in res.boxes:
                 if (int(box.cls) == 0 and float(box.conf) > threshold):
                     is_person = True
-                    thread = threading.Thread(target=send_image, args=(box,frame))
-                    thread.start()
-            # này kiểm tra nếu có người thì lấy ảnh có box còn ko có người thì lấy frame ban đầu 
+                    if (box.id[0]==None): continue # cái này để đề phòng lỗi th chứ ko có ý nghĩa gì
+                    id_person = int(box.id[0]) 
+                    confidence = float(box.conf)
+                    if id_person not in detected_person:
+                        detected_person[id_person] = confidence
+                    # nếu conf cũ nhỏ hơn thì cập nhật gửi frame mới và cập nhật conf hiện tại
+                    elif detected_person.get(id_person) < confidence:
+                        detected_person[id_person] = confidence
+                        thread = threading.Thread(target=send_image, args=(box,frame))
+                        thread.start()
+            # nếu có người thì lấy ảnh có box còn ko có người thì lấy frame ban đầu
+            # nếu muốn chạy nhanh hơn thì comment lại vì không phải show ảnh lên làm gì 
             if is_person == True:
                 res_plotted = res.plot()
             else:
@@ -86,3 +94,4 @@ if __name__ == '__main__':
             break
     cv2.destroyAllWindows()
     cam.release()
+    detected_person.clear()
